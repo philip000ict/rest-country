3e5e2084cec57b06b24238371d33f44f63d789e4
“G33ktech” <cv3tech@gmail.com>
2023-05-25 06:12:23 +0700

    Initialcommit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>REST Countries</h1>
+    <label for="countries">Choose a Country:</label><br>
+
+<select name="country" id="country">
+  <option value="country-name" v-for="item of items" :key="item.id">
+    {{ item.name.common }}
+  </option>
+</select> 
+
+  <ul>
+    <li v-for="item of items" :key="item.id">
+      <h3>{{ item.name.official}} </h3>
+      <img src="{{ item.flags.png }}" width="70" height="50"><br>
+      {{ item.flags.alt }}<br>
+      {{ item.cca2 }}<br>
+      {{ item.cca3 }}<br>
+      
+      {{ item.idd.root }}, {{ item.idd.suffixes }}<br>
+    </li>
+  </ul>
+</div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      items: [],
+      itemName: "",
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(`https://restcountries.com/v3.1/all`);
+      this.items = res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  methods: {
+    async addItem() {
+      const res = await axios.post(`https://restcountries.com/v3.1/all`, {
+        name: this.itemName,
+      });
+      this.items = [...this.items, res.data];
+      this.itemName = "";
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
