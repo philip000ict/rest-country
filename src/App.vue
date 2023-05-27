b4133ecbe195da123bdcae6d4fc737f2551606fa
“G33ktech” <cv3tech@gmail.com>
2023-05-27 14:15:30 +0700

    Sort country name list alphabetically

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,29 @@
   <div id="app">
     <div id="wrapper">
       <h1>REST Countries</h1>
-      <label for="countries">Choose a Country:</label><br>
-
+      <button type="button" @click="showflag(25)">
+        Central African Republic</button>
 <select name="country" id="country">
-  <option value="country-name" v-for="item of items" :key="item.id">
-    {{ item.name.common }}
+  <option value="country-name" v-for="item of name_list" :key="item.id">
+    {{ item }}
   </option>
 </select> <br>
 
 
     <div v-for="item of items" :key="item.id" id="country-info">
-      <button type="button" @click="displayinfo(item)"><h3>{{ item.name.official}} </h3></button>
+
+      
+
+      <h2 type="button" @click="displayinfo(item)">
+        {{ item.name.official}}</h2>
       
       <img :src="item.flags.png" width=350 height = 200 class="flag" /><br>     
       2 character Country Code: <b>{{ item.cca2 }}</b><br>
       3 character Country Code: <b>{{ item.cca3 }}</b><br>      
       International Dialling IDD <b>{{ item.idd.root }}, {{ item.idd.suffixes }}</b><br>
-      
+    
       
       Native Country Name; {{ item.name.nativeName}}
-      <button onclick="myFunction()">Click me</button>
     </div>
   </div>
 </div>
@@ -33,24 +36,49 @@ export default {
   data() {
     return {
       items: [],
-      itemName: "",
-      nativename:[]
+      ascending: true,
+      sortBy: 'alphabetically',
+      searchValue: '',
+      name_list:[]
     };
   },
   async created() {
     try {
       const res = await axios.get(`https://restcountries.com/v3.1/all`);
       this.items = res.data;
-      this.nativename = this.name.nativeName
+      this.name_list=[]
+      //this.c_names = JSON.parse(this.items)
+      for (let i = 0; i < this.items.length; i++) {
+        
+      this.name_list.push(this.items[i].name.official)
+      }
+      this.name_list.sort()
+      console.log(JSON.stringify(this.name_list))
+    
     } catch (error) {
       console.log(error);
     }
   },
   methods: {
-    displayinfo(country) {
-      alert(JSON.stringify(country, null, 4))
+    displayinfo(country) {      
+      alert(JSON.stringify(country))
     },
+    showflag(country_id){
+      this.flagbox=this.items[country_id]
+      console.log(JSON.stringify(this.flagbox, null, 2))
+      
+    }
   },
+  computed: {
+	sortedArray() {
+		let filtereditems = JSON.stringify(this.items)
+    //let filtereditems = this.items
+    console.log(filtereditems.name)
+		console.log(filtereditems)
+		
+    return filtereditems
+	}
+},
 };
 </script>
 <style>
@@ -68,5 +96,9 @@ export default {
     background-color: salmon;
 
 }
-
+  .button{
+    border: none;
+    background-color: lemonchiffon;
+    margin:auto;
+  }
 </style>
