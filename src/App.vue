158752f308ca4a454dd3ac6911c96e5a5ca85602
“G33ktech” <cv3tech@gmail.com>
2023-05-28 14:02:45 +0700

    improved text on modal pop-up

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
     <div id="wrapper">
+      <div id="navbar">
       <h1>REST Countries</h1>
-      <button type="button" @click="pagination(-25)">Back</button>
-      <button type="button" @click="pagination(25)">Next</button>
-<select name="country" id="country">
-  <option value="country-name" v-for="item of name_list" :key="item.id">
-    {{ item }}
-  </option>
-</select> <br>
-    <div v-for="item of items" :key="item.id" id="country-info">
-      <h2 type="button" @click="displayinfo(item)">
-        {{ item.name.official}}</h2>
+      <button class ="navbutton" type="button" @click="pagination(-25)">Back</button>
+      <button class ="navbutton" type="button" @click="pagination(25)">Next</button>
+
+  <select name="country" id="country" >
+    <option value="country-name" v-for="item of name_list" :key="item.id" >
+      {{ item }}
+    </option>
+  </select> <br>
+  </div>
+  <div id="flagdiv">
+    <div class="card" v-for="item of items" :key="item.id"  @click="displayinfo(item)">
+      <p class="countryName">
+        {{ item.name.official}}</p>
       
-      <img :src="item.flags.png" width=350 height = 200 class="flag" /><br>     
-      2 character Country Code: <b>{{ item.cca2 }}</b><br>
-      3 character Country Code: <b>{{ item.cca3 }}</b><br>      
-      International Dialling IDD <b>{{ item.idd.root }}, {{ item.idd.suffixes }}</b><br>
-      Native Country Name; {{ item.name.nativeName}}
+        <img :src="item.flags.png" width=350 height = 200 class="flag" /><br>     
+        2 character Country Code: <b>{{ item.cca2 }}</b><br>
+        3 character Country Code: <b>{{ item.cca3 }}</b><br>      
+        International Dialling IDD <b>{{ item.idd.root }}, {{ item.idd.suffixes }}</b>
+      </div>
     </div>
   </div>
 </div>
@@ -53,6 +57,9 @@ export default {
     increment(page){
       this.start += page
     },
+    single_country(name){
+      console.log(name)
+    },
     pagination(page){
       //this.increment(page)
       this.items.length = 0;
@@ -61,39 +68,62 @@ export default {
       if(this.start > this.all_countries.length - 25){this.start = this.all_countries.length-25}
       for (let i = this.start+0; i < this.start+25; i++) {
         this.country_index = this.index_name_list.indexOf(this.name_list[i])
-        console.log(this.name_list[i])
-        console.log(this.index_name_list.indexOf(this.name_list[i]))
         this.items.push(this.all_countries[this.country_index]); 
       }
     },
-    displayinfo(country) {      
-      alert(JSON.stringify(country))
-    },
+    displayinfo(country) {     
+      var country_string = JSON.stringify(country)
+      country_string = this.beautify(country_string)     
+      alert(country_string)
+    }, 
+    beautify(obj){
+      obj = obj.replace(/[{}]/g," ")
+      obj = obj.replace(/[":"]/g,' ')
+      obj = obj.replace(/[,]/g,'\n')
+      return obj
+    }
   },
 };
 </script>
 <style>
-  #country-info{
+  .card{
     width: 350px;
-    height: 500px;
+    height: 350px;
     padding:20px;
     background-color: powderblue;
-    margin-left: 20px;
+    margin: 20px;
     border-radius: 30px;
-    border-color: blueviolet;
-    border-width: 2px;
-    display: inline-block;
+    display:inline-block;
+    vertical-align: top;
+
 } 
+.countryName{
+  font-size: 24px;
+  text-align: center;
+}
   #wrapper{
+    margin-top: 20px;
     padding: 20px;
 }
-  .body{
-
+  .navbutton{
+    height:50px;
+    width: 100px;
+    margin: 20px;
+    background-color: powderblue;
   }
-
+#navbar{
+  background-color: aliceblue;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+  margin-bottom:20px;
+}
   .button{
     border: none;
     background-color: lemonchiffon;
-    margin:auto;
+    
+  }
+  #flagdiv{
+    padding-top: 200px;
   }
 </style>
